9afdbb71b780f2abd018491632333f60c6daee8f
Angel <angelkysinn@gmail.com>
2021-04-08 23:12:51 -0700

    Add seeds for labs and lab records for db table

---
+++ express-back-end/db/seeds/09_labs.sql
@@ -0,0 +1,6 @@
+INSERT INTO labs (name)
+VALUES ('LifeLabs Medical Laboratory Services - Kingsway'),
+('Greig Associates X-Ray, Ultrasound & Mammography Inc. - Victoria Drive'),
+('West Coast Medical Imaging - New Westminster'),
+('Downtown Radiology - Keefer Imaging'),
+('AIM Medical Imaging - West Broadway'),
