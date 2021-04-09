58cf6c0c4e0533f1708f6617713075c552da46c9
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-08 20:36:39 -0700

    Add first seed for test

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -6,10 +6,27 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const client = require("./elephantsql");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// app.get("/", function (req, res) {
+  
+//   res.send(client.query('SELECT * FROM users'));
+// });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
+});
+
+
+
+
+app.get("/", (req, res) => {
+  client.query(`SELECT * FROM users;`)
+    .then(data => {
+      const users = data.rows;
+      res.json({ users });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
 });
