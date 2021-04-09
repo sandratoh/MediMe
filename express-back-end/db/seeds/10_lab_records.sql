01458c0920e5de533e3847f3f7212709ac5b6b58
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-09 08:42:35 -0700

    Fix layout of code for consistency

--- express-back-end/db/seeds/10_lab_records.sql
+++ express-back-end/db/seeds/10_lab_records.sql
@@ -1,7 +1,7 @@
 INSERT INTO lab_records (user_id, date, referral_doctor_id, lab_id, type_of_test)
-VALUES (1, '2018-09-15', 1, 3, 'XRAY'),
-(1, '2019-03-30', 2, 1, 'BLOOD'),
-(1, '2019-03-30', 2, 1, 'URINE'),
-(1, '2020-10-18', 3, 2, 'ULTRASOUND'),
-(1, '2021-02-04', 4, 5, 'MRI')
+                 VALUES (1, '2018-09-15', 1, 3, 'XRAY'),
+                        (1, '2019-03-30', 2, 1, 'BLOOD'),
+                        (1, '2019-03-30', 2, 1, 'URINE'),
+                        (1, '2020-10-18', 3, 2, 'ULTRASOUND'),
+                        (1, '2021-02-04', 4, 5, 'MRI')
                            
