b454a475c26bae6dd39ab568fb464c34a07c5a82
Sandra To <sandra.to.e@gmail.com>
2021-04-09 15:12:26 -0700

    Add heading and body text styles based on wireframes

--- react-front-end/src/components/App.jsx
+++ react-front-end/src/components/App.jsx
@@ -3,6 +3,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>h1: This is App.jsx</h1>
+        <h2>h2: This is App.jsx</h2>
+        <h3>h3: This is App.jsx</h3>
+        <h4>h4: This is App.jsx</h4>
+        <button className="button-primary">Button Primary</button>
+        <button className="button-secondary">Button Secondary</button>
+        <button className="button-small">Button Small</button>
+        <a href='#'>Link to somewhere</a>
       </header>
     </div>
   );
