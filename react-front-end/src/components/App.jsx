04feb16770c1aca413843a607fae09708b341ee9
Sandra To <sandra.to.e@gmail.com>
2021-04-09 15:34:24 -0700

    Customize material ui font-family and use lib buttons

--- react-front-end/src/components/App.jsx
+++ react-front-end/src/components/App.jsx
@@ -1,17 +1,32 @@
+import Button from '@material-ui/core/Button';
+
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Poppins',
+      'sans-serif',
+    ].join(','),
+  },});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>h1: This is App.jsx</h1>
-        <h2>h2: This is App.jsx</h2>
-        <h3>h3: This is App.jsx</h3>
-        <h4>h4: This is App.jsx</h4>
-        <button className="button-primary">Button Primary</button>
-        <button className="button-secondary">Button Secondary</button>
-        <button className="button-small">Button Small</button>
-        <a href='#'>Link to somewhere</a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <h1>h1: This is App.jsx</h1>
+          <h2>h2: This is App.jsx</h2>
+          <h3>h3: This is App.jsx</h3>
+          <h4>h4: This is App.jsx</h4>
+          <p>p: This is App.jsx</p>
+          <div>
+            <a href='#'>Link to somewhere</a>
+          </div>
+          <Button variant="contained" color="primary">Hello World</Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
