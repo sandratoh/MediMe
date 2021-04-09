8836468bcaea3192c2c44d68b23bb8a011dfbed8
Sandra To <sandra.to.e@gmail.com>
2021-04-08 21:41:26 -0700

    Add doctor seed files

---
+++ express-back-end/db/seeds/02_doctors.sql
@@ -0,0 +1,7 @@
+INSERT INTO doctors (name)
+             VALUES ('Dr. Gale Dodek-Wenner'),
+                    ('Dr. Howard Liang'),
+                    ('Dr. Gregory House'),
+                    ('Dr. Sheldon Cooper'),
+                    ('Dr. Drake Ramoray'),
+                    ('Dr. Ross Geller');
