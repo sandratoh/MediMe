1595ccca3abd8855c1a9e789bcd5c238d1f85b0f
Angel <angelkysinn@gmail.com>
2021-04-09 10:07:52 -0700

    Update table name to clincal_visits for consistency

--- express-back-end/db/seeds/06_clinical_visits.sql
+++ express-back-end/db/seeds/06_clinical_visits.sql
@@ -1,4 +1,4 @@
-INSERT INTO clinics_visits (user_id, clinic_id, referral_doctor_id, date, type_of_visit, reason_for_visit, doctor_diagnosis)
+INSERT INTO clinical_visits (user_id, clinic_id, referral_doctor_id, date, type_of_visit, reason_for_visit, doctor_diagnosis)
                     VALUES (1, 3, 2, '2018-09-09', 'HOSPITAL', 'Stomach pain and vomit', 'Food poisoning'),
                            (1, 1, 1, '2019-02-05', 'CLINIC', 'Headache and fever', 'Seasonal flu'),
                            (1, 5, 3, '2019-04-29', 'CLINIC', 'Eye keeps twitching', 'Incontrollable eye muscle movement. Refer to opthalmalogy'),
