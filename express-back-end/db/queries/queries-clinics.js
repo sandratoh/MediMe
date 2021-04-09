a7565b66979feea38af2d544d758adc78b2a0618
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-09 15:24:35 -0700

    Logic added with helper funcs

--- express-back-end/db/queries/queries-clinics.js
+++ express-back-end/db/queries/queries-clinics.js
@@ -1,16 +1,30 @@
 const client = require("../../elephantsql");
 
-const addClinicalVisit = (cv) => {
+const getDoctorIdByName = async (name) => {
+  return client
+    .query(`SELECT id FROM doctors where name = $1`, [name])
+    .then((res) => res.rows[0].id);
+};
+
+const getClinicIdByName = async (name) => {
+  return client
+    .query(`SELECT id FROM clinics where name = $1`, [name])
+    .then((res) => res.rows[0].id);
+};
+
+const addClinicalVisit = async (cv) => {
   const query = `
       INSERT INTO clinical_visits (user_id, clinic_id, referral_doctor_id, date, type_of_visit, reason_for_visit, doctor_diagnosis)
-      VALUES ($1, (SELECT id FROM clinics WHERE name = $2), (SELECT id FROM doctors WHERE name = $3), $4, $5, $6, $7)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING *
       ;`
   
+  const referral_doctor_id = await getDoctorIdByName(cv.referral_doctor_id)
+  const clinic_id = await getClinicIdByName(cv.clinic_id)
   const values = [
     cv.user_id,
-    cv.clinic_id,
-    cv.referral_doctor_id,
+    clinic_id,
+    referral_doctor_id,
     cv.date,
     cv.type_of_visit,
     cv.reason_for_visit,
