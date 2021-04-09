25f693d586d76e1dfc9f85e238071c341bcd089e
Sandra To <sandra.to.e@gmail.com>
2021-04-09 11:11:46 -0700

    Add db reset file and scripts

--- express-back-end/elephantsql.js
+++ express-back-end/elephantsql.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const pg = require("pg");
 
-const conString = process.env.POSTGRES_URL;
+const conString = process.env.POSTGRES_URL ||
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 const client = new pg.Client(conString);
 
 client.connect(function (err) {
