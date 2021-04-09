20c7873c07b7d0b9b0c337f08c2bd544805e6fca
Angel <angelkysinn@gmail.com>
2021-04-09 13:46:49 -0700

    Add bodyParser.json()

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,53 +1,28 @@
 require("dotenv").config();
 
 // Web server config
-const express      = require("express");
-const app          = express();
-const bodyParser   = require("body-parser");
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const client = require("./elephantsql");
 
-const PORT         = process.env.PORT || 8000;
-const client       = require("./elephantsql");
+const app = express();
 
-
-
-app.use(bodyParser.urlencoded({ extend: true }));
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Seperate Routes for each Reasource
-const clinicVisitsRoutes = require('./routes/clinical_visits');
-const vaccinationsRoutes = require('./routes/vaccinations');
-const medicationsRoutes = require('./routes/medications');
-const labRecordsRoutes = require('./routes/lab_records');
+const clinicVisitsRoutes = require("./routes/clinical_visits");
+const vaccinationsRoutes = require("./routes/vaccinations");
+const medicationsRoutes = require("./routes/medications");
+const labRecordsRoutes = require("./routes/lab_records");
 
-//Mount Reasources
-app.use('/api/clinics', clinicVisitsRoutes(client))
-app.use('/api/vaccinations', vaccinationsRoutes(client))
-app.use('/api/medications', medicationsRoutes(client))
-app.use('/api/labs', labRecordsRoutes(client))
-
-
+//Mount Resources
+app.use("/api/clinics", clinicVisitsRoutes(client));
+app.use("/api/vaccinations", vaccinationsRoutes(client));
+app.use("/api/medications", medicationsRoutes(client));
+app.use("/api/labs", labRecordsRoutes(client));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
-
-
-
-
-
-//EXAMPLE
-
-// app.get("/", (req, res) => {
-//   client.query(`SELECT * FROM pharmacies;`)
-//     .then(data => {
-//       const users = data.rows;
-//       res.json({ users });
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: err.message });
-//     });
-// });
