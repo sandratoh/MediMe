f1696b6c1444f67e5e2c8e76f96c98d0b11b908b
Angel <angelkysinn@gmail.com>
2021-04-10 12:40:48 -0700

    Add route for doctors

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -17,6 +17,7 @@ const vaccinationsRoutes = require("./routes/vaccinations");
 const medicationsRoutes = require("./routes/medications");
 const labRecordsRoutes = require("./routes/lab_records");
 const usersRoutes = require("./routes/users");
+const doctorsRoutes = require("./routes/doctors");
 
 //Mount Resources
 app.use("/api/clinics", clinicVisitsRoutes(client));
@@ -24,6 +25,7 @@ app.use("/api/vaccinations", vaccinationsRoutes(client));
 app.use("/api/medications", medicationsRoutes(client));
 app.use("/api/labs", labRecordsRoutes(client));
 app.use("/api/users", usersRoutes(client));
+app.use("/api/doctors", doctorsRoutes(client));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
