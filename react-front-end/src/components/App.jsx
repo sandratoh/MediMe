2b83ee4d56bd8650c5dafbcfcc5809fd5789103b
Angel <angelkysinn@gmail.com>
2021-04-11 14:18:07 -0700

    Add route for labs

--- react-front-end/src/components/App.jsx
+++ react-front-end/src/components/App.jsx
@@ -1,6 +1,7 @@
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import ClinicsList from "./Clinics/ClinicsList";
+import LabsList from "./Labs/LabsList";
 import Header from "./Header";
 import NewVisit from "./Clinics/NewVisit";
 
@@ -29,9 +30,11 @@ function App() {
         <Router>
           <Header />
           <Link to="/clinics">Clinics</Link>
+          <Link to="/labs">Labs</Link>
           <Switch>
             <Route path="/clinics/new" component={NewVisit} />
             <Route exact path="/clinics" component={ClinicsList} />
+            <Route exact path="/labs" component={LabsList} />
           </Switch>
         </Router>
       </div>
