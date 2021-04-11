de9550c96fbbd698a3740d2fe91ef1a93ec18c5e
Angel <angelkysinn@gmail.com>
2021-04-11 14:18:52 -0700

    Add folder for Labs components. LabsCardList and LabsList working

---
+++ react-front-end/src/components/Labs/LabsCardList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Card2 from "../Card2";
+
+const lab_records = [
+  {
+    id: 1,
+    user_id: 1,
+    date: "2018-09-15T07:00:00.000Z",
+    referral_doctor_id: 1,
+    lab_id: 3,
+    type_of_test: "XRAY",
+  },
+  {
+    id: 2,
+    user_id: 1,
+    date: "2019-03-30T07:00:00.000Z",
+    referral_doctor_id: 2,
+    lab_id: 1,
+    type_of_test: "BLOOD",
+  },
+];
+
+export default function LabsCardList(props) {
+  const records = lab_records.map((lab_visit) => {
+    return (
+      <Card2
+        key={lab_visit.id}
+        className="list-items"
+        date={lab_visit.date}
+        type={lab_visit.type_of_test}
+        value={lab_visit.lab_id}
+      />
+    );
+  });
+  return <ul>{records}</ul>;
+}
---
+++ react-front-end/src/components/Labs/LabsList.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import Header from "../Header";
+import LabsCardList from "./LabsCardList";
+import IconButton from "../IconButton";
+import "./LabsList.scss";
+
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+
+const labs = [
+  {
+    id: 1,
+    name: "LifeLabs Medical Laboratory Services - Kingsway",
+  },
+  {
+    id: 2,
+    name:
+      "Greig Associates X-Ray, Ultrasound & Mammography Inc. - Victoria Drive",
+  },
+  {
+    id: 3,
+    name: "West Coast Medical Imaging - New Westminster",
+  },
+  {
+    id: 4,
+    name: "Downtown Radiology - Keefer Imaging",
+  },
+  {
+    id: 5,
+    name: "AIM Medical Imaging - West Broadway",
+  },
+];
+
+const lab_records = [
+  {
+    id: 1,
+    user_id: 1,
+    date: "2018-09-15T07:00:00.000Z",
+    referral_doctor_id: 1,
+    lab_id: 3,
+    type_of_test: "XRAY",
+  },
+  {
+    id: 2,
+    user_id: 1,
+    date: "2019-03-30T07:00:00.000Z",
+    referral_doctor_id: 2,
+    lab_id: 1,
+    type_of_test: "BLOOD",
+  },
+];
+
+export default function LabsList(props) {
+  return (
+    <section className="labs-list">
+      <Header />
+      <div className="labs-list--icons">
+        <ArrowBackIosIcon />
+        <IconButton new color="secondary" variant="contained">
+          <Link to="/labs/new">New</Link>
+        </IconButton>
+      </div>
+      <h1 className="labs-list--title">Lab Visits</h1>
+      <div className="labs-list--content">
+        <LabsCardList />
+      </div>
+    </section>
+  );
+}
