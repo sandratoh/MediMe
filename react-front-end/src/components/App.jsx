4f193b6a56b8b0dee982eeca109ee37b16f72bd2
Sandra To <sandra.to.e@gmail.com>
2021-04-11 12:22:17 -0700

    Add route to clinic new visitform

--- react-front-end/src/components/App.jsx
+++ react-front-end/src/components/App.jsx
@@ -1,7 +1,8 @@
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import ClinicsList from "./Clinics/ClinicsList";
 import Header from "./Header";
+import NewVisit from "./Clinics/NewVisit";
 
 const theme = createMuiTheme({
   typography: {
@@ -28,7 +29,10 @@ function App() {
         <Router>
           <Header />
           <Link to="/clinics">Clinics</Link>
-          <Route path="/clinics" component={ClinicsList} />
+          <Switch>
+            <Route path="/clinics/new" component={NewVisit} />
+            <Route exact path="/clinics" component={ClinicsList} />
+          </Switch>
         </Router>
       </div>
     </ThemeProvider>
