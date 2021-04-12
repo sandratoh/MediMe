a4749383bea200865ec3c6a139783f3a062039ad
Sandra To <sandra.to.e@gmail.com>
2021-04-12 12:38:55 -0700

    Set up .env for axios

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App';
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+};
 
 ReactDOM.render(
   <React.StrictMode>
