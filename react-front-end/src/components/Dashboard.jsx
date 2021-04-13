fc9a4d688df8625d7eb44a5f2b3310dc751d2430
Sandra To <sandra.to.e@gmail.com>
2021-04-13 15:02:59 -0700

    Update dashboard links to enable clickable card

--- react-front-end/src/components/Dashboard.jsx
+++ react-front-end/src/components/Dashboard.jsx
@@ -6,18 +6,26 @@ export default function Dashboard() {
 
   return (
     <section className="dashboard-card--container">
-      <DashboardCard category="clinics">
-        <Link to="/clinics">Clinics</Link>
-      </DashboardCard>
-      <DashboardCard category="labs">
-        <Link to="/labs">Labs</Link>
-      </DashboardCard>
-      <DashboardCard category="medications">
-        <Link to="/medications">Medications</Link>
-      </DashboardCard>
-      <DashboardCard category="vaccinations">
-        <Link to="/vaccinations">Vaccinations</Link>
-      </DashboardCard>
+      <Link to="/clinics">
+        <DashboardCard category="clinics">
+          Clinics
+        </DashboardCard>
+        </Link>
+      <Link to="/labs">
+        <DashboardCard category="labs">
+            Labs
+        </DashboardCard>
+      </Link>
+      <Link to="/medications">
+        <DashboardCard category="medications">
+            Medications
+        </DashboardCard>
+      </Link>
+      <Link to="/vaccinations">
+        <DashboardCard category="vaccinations">
+          Vaccinations
+        </DashboardCard>
+      </Link>
     </section>
   );
 };
