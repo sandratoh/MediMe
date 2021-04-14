f17de8cb618c065126c068efa2088472c2b5f12d
Angel <angelkysinn@gmail.com>
2021-04-13 23:31:18 -0700

    Clean up code

--- react-front-end/src/components/Vaccinations/Doses/DoseCard.jsx
+++ react-front-end/src/components/Vaccinations/Doses/DoseCard.jsx
@@ -3,43 +3,12 @@ import Typography from "@material-ui/core/Typography";
 
 import "../../../styles/card.scss";
 
-const vaccinations = [
-  {
-    id: 7,
-    user_id: 1,
-    name: "Happy Shot",
-    total_num_doses: 5,
-  },
-];
-
-const dosesForHappyShot = [
-  {
-    id: 14,
-    vaccination_id: 7,
-    serial_number: "00000000",
-    date_taken: "2021-04-01T07:00:00.000Z",
-    administration_site: "Vancouver General Hospital",
-    next_scheduled_dose: "2021-04-02T07:00:00.000Z",
-  },
-  {
-    id: 15,
-    vaccination_id: 7,
-    serial_number: "00000001",
-    date_taken: "2021-04-02T07:00:00.000Z",
-    administration_site: "Vancouver General Hospital",
-    next_scheduled_dose: "2021-04-03T07:00:00.000Z",
-  },
-];
-
 export default function DoseCard(props) {
   const formatDate = (props) => {
     const date = new Date(props.date_taken);
 
     return date.toDateString();
   };
-
-  console.log("props:", props);
-  console.log("props date_taken:", props.date_taken);
 
   return (
     <Card className="card--dose" variant="outlined">
