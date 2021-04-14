b451c090df57a186883794f798961103acd1de71
Angel <angelkysinn@gmail.com>
2021-04-13 23:11:20 -0700

    Add hard-coded data, trying to render data dynamically in component

--- react-front-end/src/components/Vaccinations/Doses/DoseCard.jsx
+++ react-front-end/src/components/Vaccinations/Doses/DoseCard.jsx
@@ -3,17 +3,54 @@ import Typography from "@material-ui/core/Typography";
 
 import "../../../styles/card.scss";
 
-export default function DoseCard() {
+const vaccinations = [
+  {
+    id: 7,
+    user_id: 1,
+    name: "Happy Shot",
+    total_num_doses: 5,
+  },
+];
+
+const dosesForHappyShot = [
+  {
+    id: 14,
+    vaccination_id: 7,
+    serial_number: "00000000",
+    date_taken: "2021-04-01T07:00:00.000Z",
+    administration_site: "Vancouver General Hospital",
+    next_scheduled_dose: "2021-04-02T07:00:00.000Z",
+  },
+  {
+    id: 15,
+    vaccination_id: 7,
+    serial_number: "00000001",
+    date_taken: "2021-04-02T07:00:00.000Z",
+    administration_site: "Vancouver General Hospital",
+    next_scheduled_dose: "2021-04-03T07:00:00.000Z",
+  },
+];
+
+export default function DoseCard(props) {
+  const formatDate = (props) => {
+    const date = new Date(props.date_taken);
+
+    return date.toDateString();
+  };
+
+  console.log("props:", props);
+  console.log("props date_taken:", props.date_taken);
+
   return (
     <Card className="card--dose" variant="outlined">
       <div className="card--details">
-        <Typography variant="subtitle1" color="textSecondary">
-          Dose #/#
+        <Typography variant="subtitle2" color="textSecondary">
+          {`Dose #/${props.total}`}
         </Typography>
       </div>
       <div className="card--details">
-        <Typography variant="subtitle1" color="textSecondary">
-          Sun Apr 12 2021
+        <Typography variant="subtitle2" color="textSecondary">
+          {formatDate(props)}
         </Typography>
       </div>
     </Card>
--- react-front-end/src/components/Vaccinations/VaccinationCard.jsx
+++ react-front-end/src/components/Vaccinations/VaccinationCard.jsx
@@ -7,7 +7,35 @@ import DoseCard from "./Doses/DoseCard";
 
 import "../../styles/card.scss";
 
-export default function SimpleAccordion() {
+const vaccinations = [
+  {
+    id: 7,
+    user_id: 1,
+    name: "Happy Shot",
+    total_num_doses: 5,
+  },
+];
+
+const dosesForHappyShot = [
+  {
+    id: 14,
+    vaccination_id: 7,
+    serial_number: "00000000",
+    date_taken: "2021-04-01T07:00:00.000Z",
+    administration_site: "Vancouver General Hospital",
+    next_scheduled_dose: "2021-04-02T07:00:00.000Z",
+  },
+  {
+    id: 15,
+    vaccination_id: 7,
+    serial_number: "00000001",
+    date_taken: "2021-04-02T07:00:00.000Z",
+    administration_site: "Vancouver General Hospital",
+    next_scheduled_dose: "2021-04-03T07:00:00.000Z",
+  },
+];
+
+export default function VaccinationCard() {
   return (
     <div className="card--vaccination">
       <Accordion>
@@ -16,11 +44,20 @@ export default function SimpleAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography variant="h6">Vaccination</Typography>
+          <Typography variant="subtitle1">{vaccinations[0].name}</Typography>
         </AccordionSummary>
         <AccordionDetails className="card--vaccination--doses">
-          <DoseCard />
-          <DoseCard />
+          {dosesForHappyShot.map}
+          <DoseCard
+            // current={}
+            total={dosesForHappyShot.length}
+            date_taken={dosesForHappyShot[0].date_taken}
+          ></DoseCard>
+          <DoseCard
+            // current={}
+            total={dosesForHappyShot.length}
+            date_taken={dosesForHappyShot[1].date_taken}
+          ></DoseCard>
         </AccordionDetails>
       </Accordion>
     </div>
