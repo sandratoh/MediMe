03cb0400a644a16d59e5888f0ca51ec7a866f890
Angel <angelkysinn@gmail.com>
2021-04-14 12:59:39 -0700

    Clean up code and remove header component

--- react-front-end/src/components/Clinics/ClinicsList.jsx
+++ react-front-end/src/components/Clinics/ClinicsList.jsx
@@ -1,33 +1,13 @@
 import { Link } from "react-router-dom";
-import Header from "../Header";
 import ClinicsCardList from "./ClinicsCardList";
 import IconButton from "../IconButton";
-import "./ClinicsList.scss";
-import { useContext, useEffect, useState } from "react";
-
-import axios from "axios";
-import { dataContext } from "../hooks/DataProvider";
 import BackButton from "../BackButton";
 
-export default function ClinicsList(props) {
-  // const { fetchAllClinics } = useContext(dataContext)
+import "./ClinicsList.scss";
 
-
-  // const [ reRender, setReRender ] = useState(false); 
-
-  // useEffect(() => {
-  //  fetchAllClinics();
-
-  //  // clean up function setReRender(false);
-  // //  return () => setReRender(false);
-
-  // }, [reRender, fetchAllClinics]);
-
-  // console.log('ClinicsList ReRender state', reRender);
-
+export default function ClinicsList() {
   return (
     <section className="clinics-list">
-      <Header />
       <div className="clinics-list--icons">
         <BackButton />
         <IconButton new color="secondary" variant="contained">
@@ -36,7 +16,6 @@ export default function ClinicsList(props) {
       </div>
       <h1 className="clinics-list--title">Clinical Visits</h1>
       <div className="clinics-list--content">
-        {/* <ClinicsCardList reRender={setReRender}/> */}
         <ClinicsCardList />
       </div>
     </section>
