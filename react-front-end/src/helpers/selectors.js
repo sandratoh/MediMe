00ab7025e75db113ae5f8af7359454bd0f83319b
Sandra To <sandra.to.e@gmail.com>
2021-04-14 02:13:52 -0700

    Remove all selectors with combined selector

--- react-front-end/src/helpers/selectors.js
+++ react-front-end/src/helpers/selectors.js
@@ -1,44 +1,11 @@
-//findClinicById
-export function findClinicById(clinics, id) {
-  let name;
-  clinics.forEach((clinic) => {
-    if (clinic.id === id) {
-      name = clinic.name;
-    }
-  });
-  return name;
-};
+/*
+ * Refactor and combine all selectors to one:
+ * findClinicById
+ * findDoctorById
+ * findLabById
+ * findVaccinationById
+*/
 
-//findDoctorById
-export function findDoctorById(doctors, id) {
-  let name;
-  doctors.forEach((doctor) => {
-    if (doctor.id === id) {
-      name = doctor.name;
-    }
-  });
-
-  return name;
-};
-
-//findLabById
-export function findLabById(labs, id) {
-  let name;
-  labs.forEach((lab) => {
-    if (lab.id === id) {
-      name = lab.name;
-    }
-  });
-  return name;
-}
-
-//findVaccinationById
-export function findVaccinationById(vaccinations, id) {
-  const vaccination = vaccinations.find(v => v.id === id);
-  return vaccination.name;
-}
-
-// Refactor and combine all selectors to one
 export function findNameById(arr, id) {
   const dataObj = arr.find(elem => elem.id === id);
   return dataObj ? dataObj.name : null;
