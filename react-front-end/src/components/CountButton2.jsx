a065692e652ce7a54cbd5516b1e2584115a02bbd
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-14 14:23:35 -0700

    Secondand final option for count buttons

---
+++ react-front-end/src/components/CountButton2.jsx
@@ -0,0 +1,37 @@
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import { useState } from 'react';
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function CountButton2() {
+  const classes = useStyles();
+  const [count, setCount] = useState(1);
+
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={() => {
+              setCount(Math.max(count - 1, 0));
+            }}>
+        < RemoveCircleIcon/>
+      </IconButton>
+      {count}
+      <IconButton onClick={() => {
+              setCount(count + 1);
+            }}>
+        < AddCircleIcon/>
+      </IconButton>
+    </div>
+  );
+}
