3a18e2ebecf2d9576201f1849999d49342dd9453
Angel <angelkysinn@gmail.com>
2021-04-16 14:04:43 -0700

    Password input component working in storybook

---
+++ react-front-end/src/stories/PasswordInput.stories.js
@@ -0,0 +1,8 @@
+import PasswordInput from "../components/Users/PasswordInput";
+
+export default {
+  title: "PasswordInput",
+  component: PasswordInput,
+};
+
+export const Password = () => <PasswordInput></PasswordInput>;
