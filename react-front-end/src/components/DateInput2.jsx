97754620284f5ad323c1fc1456de0d88d22a0ead
Angel <angelkysinn@gmail.com>
2021-04-15 23:06:29 -0700

    Add new date input component for next scheduled dose

---
+++ react-front-end/src/components/DateInput2.jsx
@@ -0,0 +1,29 @@
+// Material UI Components
+import TextField from "@material-ui/core/TextField";
+
+// Stylesheet
+import "./DateInput.scss";
+
+export default function BasicTextFields(props) {
+  return (
+    <form className="date-input--container" noValidate autoComplete="off">
+      <TextField
+        id={props.id}
+        value={props.value}
+        onChange={(event) => props.setInput(event.target.value)}
+        className="date-input--field"
+        error={!props.value && props.validate}
+        label={props.children}
+        defaultValue={new Date().toISOString().substring(0, 10)}
+        inputProps={{
+          min: new Date().toISOString().substring(0, 10),
+        }}
+        type="date"
+        size="large"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+  );
+}
