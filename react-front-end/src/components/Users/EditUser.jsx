04fb9f826498f803573dc21bb496f5c0142f76b8
Angel <angelkysinn@gmail.com>
2021-04-16 22:16:32 -0700

    Add new lb/cm text input components to edit user info form. Works with validation

--- react-front-end/src/components/Users/EditUser.jsx
+++ react-front-end/src/components/Users/EditUser.jsx
@@ -2,13 +2,12 @@
 import { useState, useContext } from "react";
 import { Redirect } from "react-router-dom";
 
-
 // Components
-import TextInput from "../TextInput";
+import TextInputLb from "../TextInputLb";
+import TextInputCm from "../TextInputCm";
 import IconButton from "../IconButton";
-import InputAdornment from '@material-ui/core/InputAdornment';
-import RhGroupedButtons from "./RhGroupButtons";
-import BloodGroupButtons from "./BloodGroupButtons"
+import RhGroupedButtons from "./RhGroupedButtons";
+import BloodGroupButtons from "./BloodGroupedButtons";
 
 // Helpers
 import { dataContext } from "../hooks/DataProvider";
@@ -17,39 +16,32 @@ import { dataContext } from "../hooks/DataProvider";
 import "../../styles/form.scss";
 
 export default function EditUser() {
-  const {
-    users,
-    setUser,
-    userEditId,
-    editUser,
-    userDetailId
-  } = useContext(dataContext);
+  const { users, setUser, userEditId, editUser, userDetailId } = useContext(
+    dataContext
+  );
 
   const user = users.find((user) => user.id === userDetailId);
- 
-  const [height, setHeight] = useState(user ? user.height_in_cm : '')
-  const [weight, setWeight] = useState(user ? user.weight_in_lb : '')
-  const [bloodType, setBloodType] = useState(user ? user.blood_type : null)
-  const [rhGroup, setRhGroup] = useState(user ? user.rh_group : null)
 
+  const [height, setHeight] = useState(user ? user.height_in_cm : "");
+  const [weight, setWeight] = useState(user ? user.weight_in_lb : "");
+  const [bloodType, setBloodType] = useState(user ? user.blood_type : null);
+  const [rhGroup, setRhGroup] = useState(user ? user.rh_group : null);
 
   // Manage redirect state based on axios call
   const [redirect, setRedirect] = useState(false);
 
   const [validate, setValidate] = useState(false);
 
-  
-
   const onCancel = () => setRedirect(true);
 
   const onSave = () => {
     const userDetail = {
-        ...user, 
-        height_in_cm: parseInt(height),
-        weight_in_lb: parseInt(weight),
-        blood_type: bloodType,
-        rh_group: rhGroup
-      };
+      ...user,
+      height_in_cm: parseInt(height),
+      weight_in_lb: parseInt(weight),
+      blood_type: bloodType,
+      rh_group: rhGroup,
+    };
 
     editUser(userDetail).then((res) => {
       res.data.error ? setValidate(true) : setRedirect(true);
@@ -62,31 +54,20 @@ export default function EditUser() {
       <h1 className="users-form--title">Your MEdi-info</h1>
       <div className="users-form--container">
         <div className="users-form--field">
-          
-          <TextInput
-            
-            value={height}
-            setInput={setHeight}
-            validate={validate}
-          >
-            Height(cm):
-          </TextInput>
-          <TextInput
-            endAdornment={<InputAdornment position="end">Lb</InputAdornment>}
-            value={weight}
-            setInput={setWeight}
-            validate={validate}
-          >
-            Weight(lb):
-          </TextInput>
-          <br/>
+          <TextInputCm value={height} setInput={setHeight} validate={validate}>
+            Height:
+          </TextInputCm>
+          <TextInputLb value={weight} setInput={setWeight} validate={validate}>
+            Weight:
+          </TextInputLb>
+          <br />
           <div className="users-form--blood-type">
             <h4>Blood Type:</h4>
             <BloodGroupButtons state={bloodType} onChange={setBloodType} />
           </div>
           <div className="users-form--rh-group">
             <h4>Rh Group:</h4>
-            <RhGroupedButtons state={rhGroup} onChange={setRhGroup}/>
+            <RhGroupedButtons state={rhGroup} onChange={setRhGroup} />
           </div>
         </div>
         <div className="users-form--user-action">
@@ -109,6 +90,5 @@ export default function EditUser() {
         </div>
       </div>
     </section>
-  )
-
+  );
 }
