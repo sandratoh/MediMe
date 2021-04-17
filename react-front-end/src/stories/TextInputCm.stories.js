fb0015d3597d1b3efa48120f6a2327ee583b8216
Angel <angelkysinn@gmail.com>
2021-04-16 22:15:39 -0700

    Add new text input components (lb/cm) for edit user info form. Works on storybook

---
+++ react-front-end/src/stories/TextInputCm.stories.js
@@ -0,0 +1,8 @@
+import TextInputCm from "../components/TextInputCm";
+
+export default {
+  title: "TextInputCm",
+  component: TextInputCm,
+};
+
+export const Cm = () => <TextInputCm></TextInputCm>;
