036f9ade1de34d556cb30b4014a2abb8fa3caaa2
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-18 11:12:12 -0700

    Button working to nav to top

--- react-front-end/src/components/NavUpButton.jsx
+++ react-front-end/src/components/NavUpButton.jsx
@@ -1,38 +1,45 @@
-
+// Libraries
+import { useRef, useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import amber from '@material-ui/core/colors/amber';
+
+// Material UI Components
 import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
 import UpIcon from '@material-ui/icons/KeyboardArrowUp';
+
+// Stylesheet
 import "./NavUpButton.scss"
-const accent = amber[400];
-
-// import UpIcon from "../images/up-arrow.png";
-
 
 export default function NavUpButton() {
+  // Timing to allow for button to load
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset)
+    }
+  }, []);
   
-const myButton = document.getElementById("button--navigate-up");
+  const myButton = document.getElementById("button--navigate-up");
 
-window.onscroll = function() {scrollFunction()};
+  window.onscroll = () => scrollFunction();
 
-function scrollFunction() {
-  if (document.documentElement.scrollTop > 20) {
-    myButton.style.display = "block";
-  } else {
-    myButton.style.display = "none";
-  }
-}
-const navUp = () => {
-  document.documentElement.scrollTop = 0;
-}
+  const scrollFunction = () => {
+    if (document.documentElement.scrollTop > 100) {
+      myButton.style.display = "block";
+    } else {
+      myButton.style.display = "none";
+    }
+  };
+
+  const navOnButton = () => {
+    document.documentElement.scrollTop = 0;
+  };
 
   return (
-    <div id="button--navigate-up" >
-      <Fab  onCLick={() => {navUp()}} id="nav--up-button" color='primary' aria-label="add">
+    <div id="button--navigate-up">
+      <Fab onClick={() => navOnButton()} color='primary' aria-label="add">
         <UpIcon />
       </Fab>
-      {/* <img  src={UpIcon} component="img" alt="button icon" /> */}
     </div>
   );
 }
