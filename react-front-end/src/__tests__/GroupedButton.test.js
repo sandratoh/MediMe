9be4e80517c907c5b1e43513b1a70b8fca9a2d0a
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-19 12:13:03 -0700

    Test for input and group button complete

--- react-front-end/src/__tests__/GroupedButton.test.js
+++ react-front-end/src/__tests__/GroupedButton.test.js
@@ -1,5 +1,5 @@
 // Libraries
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, getByTestId } from "@testing-library/react";
 
 // Component
 import ClinicGroupedButtons from "../components/Clinics/ClinicGroupedButtons";
@@ -8,12 +8,36 @@ afterEach(cleanup);
 
 describe("Group Text Button", () => {
 
-  const clinicGroupArray = [
-    { id: 1, name: "HOSPITAL" },
-    { id: 2, name: "CLINIC" },
-  ];
-
   it("renders without crashing", () => {
     render(<ClinicGroupedButtons />)
   })
+
+  it("renders a HOSPITAL and CLINC button", () => {
+    const { getByText } = render(<ClinicGroupedButtons />);
+    expect(getByText("HOSPITAL" && "CLINIC")).toBeInTheDocument();
+  })
+
+// not working, uses the span inside of the class div level :(
+  // it("renders coontained style once state changes", () => {
+    
+  //   const { getByText, debug } = render(<ClinicGroupedButtons state="HOSPITAL" />);
+  //   const button1 = getByText("HOSPITAL");
+  //   const button2 = getByText("CLINIC");
+  //   debug()
+
+  //   expect(button1).toHaveClass("MuiButton-contained")
+  //   expect(button2).toHaveClass("MuiButton-outlined")
+  // })
+
+  it("renders a clickable button", () => {
+    const handleChange = jest.fn();
+    const { getByText } = render(<ClinicGroupedButtons onChange={handleChange} />)
+    const button = getByText("HOSPITAL")
+    fireEvent.click(button)
+    expect(handleChange).toHaveBeenCalledTimes(1); 
+  })
+
+
+
+
 })
