77801a73e9d5c9ca3c48e87fa26b0a9cfe72e674
Sandra To <sandra.to.e@gmail.com>
2021-04-19 12:44:50 -0700

    Add edit record cypress test -- back btn not detected

--- react-front-end/cypress/integration/labs.spec.js
+++ react-front-end/cypress/integration/labs.spec.js
@@ -28,54 +28,68 @@ describe("User can add, edit, and delete lab records", () => {
 
     cy.get("[data-testid=lab-name]")
       // .get("textarea").first()
-      .type("Dexter's Laboratory")
+      .type("Dexter's Laboratory");
 
-    cy.get("[data-testid=lab-record-type")
-      .click()
+    cy.get("[data-testid=lab-record-type]")
+      .click();
 
     cy.contains("MRI")
-      .click()
+      .click();
 
-    cy.get("[data-testid=lab-doctor")
-      .type("Dr. Janice Flamingo")
+    cy.get("[data-testid=lab-doctor]")
+      .type("Dr. Janice Flamingo");
 
     // 4. Press save
     cy.contains(/save/i)
-      .click()
+      .click();
 
     // 5. Expect to contain new record
-    cy.contains(".card", "Mar 01 2024")
-    cy.contains(".card", "Dexter's Laboratory")
+    cy.contains(".card", "Mar 01 2024");
+    cy.contains(".card", "Dexter's Laboratory");
 
   });
 
-  xit("should edit a lab record", () => {
+  it("should edit a lab record", () => {
     // 1. Click into lab records
-
+    cy.wait(1000);
+    cy.contains("Lab Records").click();
 
     // 2. Click on first lab record
-
+    cy.contains(".card", "Dexter's Laboratory")
+      .click();
 
     // 3. Expect to see record detail page
-
-
     // 4. Click on edit
-
-
+    cy.contains(/edit/i)
+      .click();
+      
     // 5. Update record
+    cy.contains("Dexter's Laboratory")
+      .clear()  
+      .type("Dexter's New Lab");
 
-
+    cy.contains("MRI")
+      .click();
     // 6. Click on submit
+    cy.contains("Ultrasound")
+      .click();
 
-
+    cy.contains(/save/i)
+      .click();
     // 7. Expect to see new updated page
+    cy.get(".lab-detail--data")
+      .contains("Dexter's New Lab");
 
+    cy.get(".lab-detail--data")
+      .contains(/ultrasound/i);
 
     // 8. Go back to list
-
+    // cy.get("span")
+    //   .contains(".back-button")
+    //   .click()
 
     // 9. Expect to see new updated record in list
-
+    // cy.contains(".card", "Dexter's New Lab")
   });
 
   xit("should delete a lab record", () => {
