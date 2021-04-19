dfb25d2493f396a199889d6b3c1c276fd3acbd16
Sandra To <sandra.to.e@gmail.com>
2021-04-19 00:53:57 -0700

    Add device viewport and pseudocode for tests

--- react-front-end/cypress/integration/labs.spec.js
+++ react-front-end/cypress/integration/labs.spec.js
@@ -1,5 +1,7 @@
 describe("User can add, edit, and delete lab records", () => {
   beforeEach(() => {
+    cy.viewport("iphone-6+");
+
     cy.visit("/");
 
     cy.contains(/log in/i).click();
@@ -12,14 +14,69 @@ describe("User can add, edit, and delete lab records", () => {
   });
   
   it("should add a lab record", () => {
+    // 1. Click into lab records
+
+
+    // 2. Click new button
+
+
+    // 3. Enter info into fields
+
+
+    // 4. Press save
+
+
+    // 5. Expect to contain new record
 
   });
 
-  it("should edit a lab record", () => {
+  xit("should edit a lab record", () => {
+    // 1. Click into lab records
+
+
+    // 2. Click on first lab record
+
+
+    // 3. Expect to see record detail page
+
+
+    // 4. Click on edit
+
+
+    // 5. Update record
+
+
+    // 6. Click on submit
+
+
+    // 7. Expect to see new updated page
+
+
+    // 8. Go back to list
+
+
+    // 9. Expect to see new updated record in list
 
   });
 
-  it("should delete a lab record", () => {
+  xit("should delete a lab record", () => {
+    // 1. Click on lab record
+
+
+    // 2. Find the first lab record
+
+
+    // 3. Expect to see detail page
+
+
+    // 4. Click on delete
+
+
+    // 5. Expect to be redirected back to list
+
+
+    // 6. Expect to not find deleted record
+
 
   });
 });
