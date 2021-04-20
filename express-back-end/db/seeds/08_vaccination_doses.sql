ae11fe6bfa45209d3b40a7a85e52be26606311b6
Sandra To <sandra.to.e@gmail.com>
2021-04-20 21:34:58 +0000

    Fix seed file date error

--- express-back-end/db/seeds/08_vaccination_doses.sql
+++ express-back-end/db/seeds/08_vaccination_doses.sql
@@ -5,6 +5,6 @@ INSERT INTO vaccination_doses (vaccination_id, serial_number, date_taken, admini
                               (4, 'AS8DXJ3KSS0', '2020-11-21', 'Vancouver General Hospital', null),
                               (5, 'DK92JSRAL94', '2019-10-22', 'Shoppers Drug Mart at Main', '2020-01-30'),
                               (6, 'SBJS09DK22S', '2019-06-19', 'McCue Pharmacy', null),
-                              (6, 'QLWD202KSLL', '2018-01-12', 'Pure Integrative Pharmacy', '2019-22-30'),
+                              (6, 'QLWD202KSLL', '2018-01-12', 'Pure Integrative Pharmacy', '2019-12-30'),
                               (7, 'AS98JSN2SJL', '2017-03-21', 'The Medicine Shoppe Pharmacy', '2018-06-30'),
                               (7, 'NNMD2KDKS03', '2016-11-23', 'Minoru Pharmacy Ltd', '2017-05-22');
