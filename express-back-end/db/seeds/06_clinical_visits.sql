507059165b3a241fd5b03f6db87fa88fbccf2ecb
Sandra To <sandra.to.e@gmail.com>
2021-04-21 16:59:27 -0700

    Update clinic id to reflect hospital icon

--- express-back-end/db/seeds/06_clinical_visits.sql
+++ express-back-end/db/seeds/06_clinical_visits.sql
@@ -4,5 +4,5 @@ INSERT INTO clinical_visits (user_id, clinic_id, doctor_id, date, type_of_visit,
                            (1, 5, 3, '2019-04-29', 'CLINIC', 'Eye keeps twitching', 'Incontrollable eye muscle movement. Refer to opthalmalogy'),
                            (1, 4, 4, '2020-03-07', 'HOSPITAL', 'Broke my thumb while typing', 'Broken thumb. Requisition to X-Ray'),
                            (1, 2, 5, '2021-01-02', 'CLINIC', 'Measle vaccine overdue', 'Received MMR vaccines'),
-                           (1, 6, 6, '2021-04-11', 'HOSPITAL', 'Fell off the stairs', 'Broken ankle, crutches for 2 weeks'),
-                           (1, 6, 6, '2021-04-11', 'HOSPITAL', 'Fell off the stairs', 'Broken ankle, crutches for 2 weeks');
+                           (1, 7, 6, '2021-04-11', 'HOSPITAL', 'Fell off the stairs', 'Broken ankle, crutches for 2 weeks'),
+                           (1, 7, 6, '2021-04-11', 'HOSPITAL', 'Fell off the stairs', 'Broken ankle, crutches for 2 weeks');
